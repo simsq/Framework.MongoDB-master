3c78378fc06c7523a24b87a3ade39414a2953145
Jason <simsq@vip.qq.com>
2017-12-13 18:20:03 +0800

    测试

--- LogClient/Program.cs
+++ LogClient/Program.cs
@@ -8,7 +8,7 @@ namespace LogClient
         static void Main(string[] args)
         {
             for (int i = 0; i < 100; i++)
-            { 
+            {   
                 LogHelper.Info("提示去外地群多无", "提示去外地群多无");
                 LogHelper.Error("提示去外地群多无", new Exception("提示去外地群多无").ToString());
             }
